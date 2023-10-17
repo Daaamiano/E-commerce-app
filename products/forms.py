8ecd123acdb9f1c4e3ecfc8d98e1fa6dfad05a89
Damian <damianmaciagowski@outlook.com>
2023-10-17 20:53:57 +0200

    removed unused registration form

--- products/forms.py
+++ products/forms.py
@@ -2,12 +2,6 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from .models import Role
 
-class RegistrationForm(UserCreationForm):
-    class Meta:
-        model = Role
-        fields = ['username', 'password1', 'password2', 'role']
-
-
 class LoginForm(forms.Form):
     username = forms.CharField(label='Nazwa użytkownika')
     password = forms.CharField(label='Hasło', widget=forms.PasswordInput)
